5393ce95ac71af05319ba41a2e6e1f512f0cf830
Abeldamj <abeldamj@gmail.com>
2023-07-20 18:51:47 +0200

    Ajout de page

--- lib/main.dart
+++ lib/main.dart
@@ -1,155 +1,83 @@
-import 'dart:convert';
+[16:04, 20/07/2023] Paccioli: import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:quotes/detail.dart';
 
 final dio = Dio();
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    Widget citationSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
+@override
+Widget build(BuildContext context) {
+Widget titleSection = Container(
+padding: const EdgeInsets.all(32),
+child: Row(
+children: [
+Expanded(
+child: Column(
+crossAxisAlignment: CrossAxisAlignment.end,
+children: [
+Container(
+padding: const EdgeInsets.only(bottom: 8),
+child: const Text(
+'Oeschinen Lake Campground',
+s…
+[16:05, 20/07/2023] Paccioli: Tu crée ensuite un fichier detail.dart
+[16:05, 20/07/2023] Paccioli: et tu mets ceci
+[16:05, 20/07/2023] Paccioli: import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'main.dart';
+class DetailCitation extends StatefulWidget {
+String id;
+DetailCitation({Key? key, required this.id}) : super(key: key);
 
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Date de publication',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-
-    return MaterialApp(
-      title: 'Flutter citations',
-      theme: ThemeData(
-          primarySwatch: Colors.green
-      ),
-      home: const MyHomePage(),
-    );
-  }
+@override
+State<DetailCitation> createState() => _DetailCitationState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class _DetailCitationState extends State<DetailCitation> {
+final dio = Dio();
+var jsonInfo;
+@override
+void initState() {
+getCitationInfo(widget.id);
 }
 
-class QuoteInfoPage extends StatelessWidget {
-  const QuoteInfoPage({Key? key}) :super(key: key);
+getCitationInfo(String id) async {
+var response = await dio.get('https://api.quotable.io/quotes/${id}');
+print("33333333333333333333333333333333333333333333 ${response}");
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Citation'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
-    );
-  }
+if (response.statusCode == 200) {
+setState(() {
+jsonInfo = response.data;
+});
+} else {
+print(response.statusCode);
+}
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+static const routeName = '/citationInfo';
 
-  var jsonList;
-//   var jsonLength;
-  @override
-  void initState() {
-    getCitations();
-  }
-
-
-  void getCitations() async {
-    try {
-      var response = await dio.get('https://api.quotable.io/quotes');
-      if (response.statusCode == 200) {
-        setState(() {
-//           jsonLength = response.data["count"];
-          jsonList = response.data['results'] as List;
-        });
-//         print(response);
-      } else {
-        print(response.statusCode);
-      }
-    }
-    catch(e){
-      print(e);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Our citations'),
-      ),
-      body: ListView.builder(
-          itemCount: jsonList == null ? 0 : jsonList.length,
-          itemBuilder: (BuildContext context, int index){
-            return Card(
-                child: ListTile(
-                  /*leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(80),
-                      child: Image.network(
-                        jsonList[index],
-                        fit: BoxFit.fill,
-                        width: 50,
-                        height: 50,
-                      ),
-                    )*/
-                    title: Text(jsonList[index]['content']),
-                    subtitle: Text(jsonList[index]['author']),
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const QuoteInfoPage()),
-                      );
-                    }
-                ));
-          }),
-    );
-  }
+Container citationSection () {
+return Container(
+child: Padding(
+padding: EdgeInsets.all(32),
+child:jsonInfo == null ? null :  Text(
+jsonInfo['content'],
+softWrap: true,
+),
+));
+}
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: jsonInfo == null ? null :  Text(jsonInfo['author']),
+),
+body: citationSection (),
+);
+}
 }
